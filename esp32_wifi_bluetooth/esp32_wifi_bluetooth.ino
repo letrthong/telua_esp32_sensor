654e81482152748f5d26ec20d551167cb35c8732
Thong LT <letrthong@gmail.com>
2023-05-20 15:44:09 +0700

    Update esp32_wifi_bluetooth.ino

--- esp32_wifi_bluetooth/esp32_wifi_bluetooth.ino
+++ esp32_wifi_bluetooth/esp32_wifi_bluetooth.ino
@@ -3,13 +3,18 @@
 
 #include <HTTPClient.h>
 
-const char* ssid = "hcmus";   /*SSID of network to connect*/
-const char* password = "fetelxxx";  /*password for SSID*/
+const char* ssid = "TP-Link_2878";   /*SSID of network to connect*/
+const char* password = "51521264";   /*password for SSID*/
+const char email = "letrthong@gmail.com"
 
 String serverName = "http://34.111.197.130:80/service/v1/esp32/update-sensor";
 
 unsigned long previousMillis = 0;
 unsigned long interval = 30000;
+
+
+Adafruit_SHT4x sht4 = Adafruit_SHT4x();
+
 
 
 void initWiFi() {
@@ -19,11 +24,58 @@ void initWiFi() {
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
     delay(1000);
+    break;
   }
   Serial.println(WiFi.localIP());
 }
   
-
+void initSht4x(){
+   Serial.println("Adafruit SHT4x test");
+  if (! sht4.begin()) {
+    Serial.println("Couldn't find SHT4x");
+  } else {
+        Serial.println("Found SHT4x sensor");
+        Serial.print("Serial number 0x");
+        Serial.println(sht4.readSerial(), HEX);
+      
+        // You can have 3 different precisions, higher precision takes longer
+        sht4.setPrecision(SHT4X_HIGH_PRECISION);
+        switch (sht4.getPrecision()) {
+           case SHT4X_HIGH_PRECISION: 
+             Serial.println("High precision");
+             break;
+           case SHT4X_MED_PRECISION: 
+             Serial.println("Med precision");
+             break;
+           case SHT4X_LOW_PRECISION: 
+             Serial.println("Low precision");
+             break;
+        }
+      switch (sht4.getHeater()) {
+       case SHT4X_NO_HEATER: 
+         Serial.println("No heater");
+         break;
+       case SHT4X_HIGH_HEATER_1S: 
+         Serial.println("High heat for 1 second");
+         break;
+       case SHT4X_HIGH_HEATER_100MS: 
+         Serial.println("High heat for 0.1 second");
+         break;
+       case SHT4X_MED_HEATER_1S: 
+         Serial.println("Medium heat for 1 second");
+         break;
+       case SHT4X_MED_HEATER_100MS: 
+         Serial.println("Medium heat for 0.1 second");
+         break;
+       case SHT4X_LOW_HEATER_1S: 
+         Serial.println("Low heat for 1 second");
+         break;
+       case SHT4X_LOW_HEATER_100MS: 
+         Serial.println("Low heat for 0.1 second");
+         break;
+    }
+  }
+}
  
 void setup() {
   // put your setup code here, to run once:
@@ -32,6 +84,8 @@ void setup() {
   initWiFi();
   Serial.print("RSSI (WiFi strength): ");
   Serial.println(WiFi.RSSI());
+
+  initSht4x();
    
 }
 
@@ -42,13 +96,28 @@ void loop() {
   /*if condition to check wifi reconnection*/
   if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >=interval)) {
     Serial.print(millis());
-    Serial.println("Reconnecting to WiFi...");
+    Serial.println("  Reconnecting to WiFi...");
     WiFi.disconnect();
+    delay(1000);
     WiFi.reconnect();
     previousMillis = currentMillis;
-  }else if ((currentMillis - previousMillis >=interval)){
+  } else if ((currentMillis - previousMillis >=interval)){
+      sensors_event_t humidity, temp;
+      
+      uint32_t timestamp = millis();
+      sht4.getEvent(&humidity, &temp);
+
+      Serial.print("Temperature: ");
+      Serial.print(temp.temperature);
+      Serial.println(" degrees C");
+      Serial.print("Humidity: "); 
+      Serial.print(humidity.relative_humidity);
+      Serial.println("% rH");
+
+      String temperature =  String(temp.temperature, 2);
+      
       HTTPClient http; 
-      String serverPath = serverName + "?temperature=24.37";
+      String serverPath = serverName + "?temperature=" + temperature;
       http.begin(serverPath.c_str());
       
       // Send HTTP GET request
@@ -59,8 +128,7 @@ void loop() {
         Serial.println(httpResponseCode);
         String payload = http.getString();
         Serial.println(payload);
-      }
-      else {
+      } else {
         Serial.print("Error code: ");
         Serial.println(httpResponseCode);
       }
