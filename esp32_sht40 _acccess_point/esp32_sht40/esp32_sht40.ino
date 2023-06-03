72da7acf58c8c1244155dcb1b383089a33fa5950
Thong LT <letrthong@gmail.com>
2023-06-03 23:32:17 +0700

    Update esp32_sht40.ino

--- esp32_sht40 _acccess_point/esp32_sht40/esp32_sht40.ino
+++ esp32_sht40 _acccess_point/esp32_sht40/esp32_sht40.ino
@@ -66,85 +66,85 @@ void startLocalWeb(){
     while(1){
        WiFiClient client = server.available(); 
        if (client) {                             // If a new client connects,
-        Serial.println("New Client.");          // print a message out in the serial port
-        String currentLine = "";                // make a String to hold incoming data from the client
-        bool hasWrongFormat = false;
-        while (client.connected()) {            // loop while the client's connected
-          if (client.available()) {             // if there's bytes to read from the client,
-            char c = client.read();             // read a byte, then
-//            Serial.write(c);                    // print it out the serial monitor
-            header += c;
-            if (c == '\n') {                    // if the byte is a newline character
-              // if the current line is blank, you got two newline characters in a row.
-              // that's the end of the client HTTP request, so send a response:
-              if (currentLine.length() == 0) {
-                // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-                // and a content-type so the client knows what's coming, then a blank line:
-                client.println("HTTP/1.1 200 OK");
-                client.println("Content-type:text/html");
-                client.println("Connection: close");
-                client.println();
-                
-                int index  = header.indexOf("/router_info?ssid=");
-                if ( index>= 0) {
-                    String parameters =  header.substring(index + 18);
-                    index  = parameters.indexOf("HTTP");
-                    
-                    if(index > 0){
-                       String  info =  parameters.substring(0,index);
-                       Serial.println("/router_info");
-                       Serial.println(info);
-                       index = info.indexOf("&password="); 
-                        
-                       String ssid = info.substring(0,index);
-                       String passowrd= info.substring(index+10);
-                       passowrd.replace(" ",  "");
-                       
-                       Serial.print("ssid=[");
-                       Serial.print(ssid);
-                       Serial.print("]");
-                       
-                       Serial.print("passowrd=[");
-                       Serial.print(passowrd);
-                       Serial.print("]");
-
-                      if(ssid.length()>0 && passowrd >= 8){
-                        
-                      }else{
-                        hasWrongFormat = true;
-                      }
-                       
-                   }
-                }  
-                
-                // Display the HTML web page
-                client.println("<!DOCTYPE html><html>");
-                client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-                client.println("<link rel=\"icon\" href=\"data:,\">");   
-                // Web Page Heading
-                client.println("<body><h4>Telua IoT platformr</h4>");
-                client.println("<form action=\"/router_info\"  method=\"get\">");
-                client.println(" <label for=\"ssid\">SSID Cua WI-Fi</label><br>");
-                client.println("<input type=\"text\" id=\"ssid\" name=\"ssid\" value=\"\"><br>");
-                 client.println(" <label for=\"password\">Mat Khau Cua WI-Fi</label><br>");
-                client.println("<input type=\"text\" id=\"password\" name=\"password\" value=\"\"><br>");
-                client.println("<input type=\"submit\" value=\"Xác Nhận\">");
-                client.println("</form>");
-                if( hasWrongFormat == true){
-                   client.println("<p>Xin kiem tra lai SSID va Mat Khau của Wi-Fi</p>");
+          Serial.println("New Client.");          // print a message out in the serial port
+          String currentLine = "";                // make a String to hold incoming data from the client
+          bool hasWrongFormat = false;
+          while (client.connected()) {            // loop while the client's connected
+            if (client.available()) {             // if there's bytes to read from the client,
+              char c = client.read();             // read a byte, then
+  //            Serial.write(c);                    // print it out the serial monitor
+              header += c;
+              if (c == '\n') {                    // if the byte is a newline character
+                // if the current line is blank, you got two newline characters in a row.
+                // that's the end of the client HTTP request, so send a response:
+                if (currentLine.length() == 0) {
+                  // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                  // and a content-type so the client knows what's coming, then a blank line:
+                  client.println("HTTP/1.1 200 OK");
+                  client.println("Content-type:text/html");
+                  client.println("Connection: close");
+                  client.println();
+                  
+                  int index  = header.indexOf("/router_info?ssid=");
+                  if ( index>= 0) {
+                      String parameters =  header.substring(index + 18);
+                      index  = parameters.indexOf("HTTP");
+                      
+                      if(index > 0){
+                         String  info =  parameters.substring(0,index);
+                         Serial.println("/router_info");
+                         Serial.println(info);
+                         index = info.indexOf("&password="); 
+                          
+                         String ssid = info.substring(0,index);
+                         String passowrd= info.substring(index+10);
+                         passowrd.replace(" ",  "");
+                         
+                         Serial.print("ssid=[");
+                         Serial.print(ssid);
+                         Serial.print("]");
+                         
+                         Serial.print("passowrd=[");
+                         Serial.print(passowrd);
+                         Serial.print("]");
+  
+                        if(ssid.length()>0 && passowrd >= 8){
+                          
+                        }else{
+                          hasWrongFormat = true;
+                        }
+                         
+                     }
+                  }  
+                  
+                  // Display the HTML web page
+                  client.println("<!DOCTYPE html><html>");
+                  client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+                  client.println("<link rel=\"icon\" href=\"data:,\">");   
+                  // Web Page Heading
+                  client.println("<body><h4>Telua IoT platformr</h4>");
+                  client.println("<form action=\"/router_info\"  method=\"get\">");
+                  client.println(" <label for=\"ssid\">SSID Cua WI-Fi</label><br>");
+                  client.println("<input type=\"text\" id=\"ssid\" name=\"ssid\" value=\"\"><br>");
+                   client.println(" <label for=\"password\">Mat Khau Cua WI-Fi</label><br>");
+                  client.println("<input type=\"text\" id=\"password\" name=\"password\" value=\"\"><br>");
+                  client.println("<input type=\"submit\" value=\"Xác Nhận\">");
+                  client.println("</form>");
+                  if( hasWrongFormat == true){
+                     client.println("<p>Xin kiem tra lai SSID va Mat Khau của Wi-Fi</p>");
+                  }
+                  client.println("</body></html>");
+                  
+                  // The HTTP response ends with another blank line
+                  client.println();
+                  // Break out of the while loop
+                  break;
+                } else { // if you got a newline, then clear currentLine
+                  currentLine = "";
                 }
-                client.println("</body></html>");
-                
-                // The HTTP response ends with another blank line
-                client.println();
-                // Break out of the while loop
-                break;
-              } else { // if you got a newline, then clear currentLine
-                currentLine = "";
+              } else if (c != '\r') {  // if you got anything else but a carriage return character,
+                currentLine += c;      // add it to the end of the currentLine
               }
-            } else if (c != '\r') {  // if you got anything else but a carriage return character,
-              currentLine += c;      // add it to the end of the currentLine
-            }
           }
         }
         // Clear the header variable
