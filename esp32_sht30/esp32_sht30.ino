4d894b8414aa1908a4bae6f4fea7edd983716fdf
Thong LT <letrthong@gmail.com>
2023-06-08 06:56:57 +0700

    Update esp32_sht30.ino

--- esp32_sht30/esp32_sht30.ino
+++ esp32_sht30/esp32_sht30.ino
@@ -81,11 +81,11 @@ void startLocalWeb(){
             count = count +1;
             if(hasConnection == true  ){
               hasConnection = false;
-               count = 9;
+               count = 5;
             }
        }
 
-       if(  count >= 10){
+       if(  count >= 6){
            server.close();
            WiFi.disconnect();
            delay(100);
