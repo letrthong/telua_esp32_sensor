030bfcae200883450930a3f1f5f0efe0f01aaa73
Thong LT <letrthong@gmail.com>
2023-06-23 14:01:42 +0700

    Update esp32_sgp40.ino

--- esp32_sgp40_sht40/esp32_sgp40/esp32_sgp40.ino
+++ esp32_sgp40_sht40/esp32_sgp40/esp32_sgp40.ino
@@ -16,8 +16,8 @@
  #define TIME_TO_SLEEP 30
 
 
-RTC_DATA_ATTR int bootCount = 0;
-RTC_DATA_ATTR bool isCorrectPassword = false;
+ 
+bool isCorrectPassword = false;
 
 
  String deviceID = "";
@@ -33,11 +33,12 @@ RTC_DATA_ATTR bool isCorrectPassword = false;
 unsigned long previousMillis = 0;
 unsigned long interval = 30000;
 
-int32_t voc_Index =0;
+int  led_index =0;
+
  
- bool hasRouter = false;
- bool hasSensor = false;
- int time_to_sleep_mode = TIME_TO_SLEEP;
+bool hasRouter = false;
+bool hasSensor = false;
+int time_to_sleep_mode = TIME_TO_SLEEP;
 
 Adafruit_SHT4x sht4 = Adafruit_SHT4x();
 Adafruit_SGP40 sgp;
@@ -53,18 +54,17 @@ String g_ssid = "";
 String ssid_list = "";
 
 void intGpio(){
-  pinMode(ledBlue, OUTPUT);
-  digitalWrite(ledBlue, LOW);
-
-  pinMode(ledGreen, OUTPUT);
-  digitalWrite(ledGreen, LOW);
-
-   pinMode(ledYellow, OUTPUT);
-  digitalWrite(ledYellow, LOW);
-
-  pinMode(ledRed, OUTPUT);
-  digitalWrite(ledRed, LOW);
-  
+    pinMode(ledBlue, OUTPUT);
+    digitalWrite(ledBlue, LOW);
+    
+    pinMode(ledGreen, OUTPUT);
+    digitalWrite(ledGreen, LOW);
+    
+    pinMode(ledYellow, OUTPUT);
+    digitalWrite(ledYellow, LOW);
+    
+    pinMode(ledRed, OUTPUT);
+    digitalWrite(ledRed, LOW);
 }
 
 void turnOnNotification(int led){
@@ -75,15 +75,15 @@ void turnOnNotification(int led){
    }else  if( led == 1){
        digitalWrite(ledGreen, LOW);
         delay(500);
-        digitalWrite(ledBlue, HIGH);
+        digitalWrite(ledGreen, HIGH);
    }else  if( led == 2){
        digitalWrite(ledYellow, LOW);
          delay(500);
-        digitalWrite(ledBlue, HIGH);
+        digitalWrite(ledYellow, HIGH);
    }else  if( led == 3){
       digitalWrite(ledRed, LOW);
        delay(500);
-       digitalWrite(ledBlue, HIGH);
+       digitalWrite(ledRed, HIGH);
    }
     delay(500);
 }
@@ -270,228 +270,106 @@ void startLocalWeb(){
        }
     }
 }
- void initWiFi() {
-   WiFi.mode(WIFI_STA);
-
-   String current_ssid = EEPROM.readString(EEPROM_ADDRESS_SSID);
-   String current_pass = EEPROM.readString(EEPROM_ADDRESS_PASS);
-   unsigned int  length_of_ssid  = current_ssid.length();
-   g_ssid = current_ssid;
-   hasRouter = false;
-
-    if(isCorrectPassword == false){
-        for(int y = 0; y< 3; y++){
-        ssid_list = "";
-        int n = WiFi.scanNetworks();
-        Serial.println("Scan done");
-         if (n == 0) {
-           Serial.println("no networks found");
-         } else {
-           Serial.print(n);
-           Serial.println(" networks found");
-           for (int i = 0; i < n; ++i) {
-             String SSID = WiFi.SSID(i);
-             Serial.print("scanNetworks SSID=");
-             Serial.println(SSID);
-             if( i < 5){
-                  ssid_list = ssid_list + SSID +  ",";
-              }
-              
-             if (length_of_ssid > 0) {
-               if (current_ssid.equals(SSID)) {
-                 hasRouter = true;
-                  Serial.println("scanNetworks hasRouter");
-                 break;
-               }
+ void reportCloud(String  temperature, String relative_humidity, String  str_voc_index) {
+       Serial.println("reportCloud start");
+       WiFi.mode(WIFI_STA);
+       String current_ssid = EEPROM.readString(EEPROM_ADDRESS_SSID);
+       String current_pass = EEPROM.readString(EEPROM_ADDRESS_PASS);
+       unsigned int  length_of_ssid  = current_ssid.length();
+       g_ssid = current_ssid;
+       //hasRouter = false;
+    
+      if(isCorrectPassword == false){
+          for(int y = 0; y< 3; y++){
+              ssid_list = "";
+              int n = WiFi.scanNetworks();
+              Serial.println("Scan done");
+               if (n == 0) {
+                 Serial.println("no networks found");
+               } else {
+                 Serial.print(n);
+                 Serial.println(" networks found");
+                 for (int i = 0; i < n; ++i) {
+                   String SSID = WiFi.SSID(i);
+                   Serial.print("scanNetworks SSID=");
+                   Serial.println(SSID);
+                   if( i < 5){
+                        ssid_list = ssid_list + SSID +  ",";
+                    }
+                    
+                   if (length_of_ssid > 0) {
+                     if (current_ssid.equals(SSID)) {
+                       hasRouter = true;
+                        Serial.println("scanNetworks hasRouter");
+                       break;
+                     }
+                   }
+             }
+        
+             int len = ssid_list.length();
+             if(len >1){
+                ssid_list = ssid_list.substring(0,len-1);
              }
            }
-      
-           int len = ssid_list.length();
-           if(len >1){
-              ssid_list = ssid_list.substring(0,len-1);
+           WiFi.scanDelete();
+    
+           if(hasRouter == true){
+            break;
            }
+           delay(500);
+       }
+
+     }
+       if (hasRouter == true  || isCorrectPassword == true) {
+         WiFi.begin(current_ssid, current_pass);
+         Serial.println("Connecting to WiFi ..");
+         int countWifiStatus = 0;
+         int retryTime = 30;
+         if(isCorrectPassword == true){
+            retryTime = 60;
          }
-         WiFi.scanDelete();
-  
-         if(hasRouter == true){
-          break;
-         }
-         delay(500);
-     }
-    }
-   
-
-   if (hasRouter == true || isCorrectPassword == true) {
-     WiFi.begin(current_ssid, current_pass);
-     Serial.println("Connecting to WiFi ..");
-     int countWifiStatus = 0;
-     int retryTime = 30;
-     if(isCorrectPassword == true){
-        retryTime = 60;
-     }
-     while (WiFi.status() != WL_CONNECTED) {
-       Serial.print('.');
-       delay(500);
-        // 15 seconds
-       countWifiStatus = countWifiStatus + 1;
-       if (countWifiStatus  > retryTime  ) {
-         break;
+          while (WiFi.status() != WL_CONNECTED) {
+           Serial.print('.');
+           delay(500);
+            // 15 seconds
+           countWifiStatus = countWifiStatus + 1;
+           if (countWifiStatus  > retryTime  ) {
+             break;
+           } 
+          }
+          
+          if(WiFi.status() == WL_CONNECTED){
+            isCorrectPassword = true;
+          }
        }
-        
-     }
-
-     if(WiFi.status() == WL_CONNECTED){
-        isCorrectPassword = true;
-     }
-   }
-    Serial.println(WiFi.localIP());
-   if (WiFi.status() != WL_CONNECTED && isCorrectPassword == false){
-      startLocalWeb();
-  } 
- }
-
- void turnOffWiFi() {
-   WiFi.disconnect();
- }
-
- void initSht4x() {
-   Serial.println("Telua SHT4x test");
-    
-  if (! sgp.begin()){
-    Serial.println("SGP40 sensor not found :(");
-    return;
-  }else{ 
-        Serial.print("Found SGP40 serial #");
-        Serial.print(sgp.serialnumber[0], HEX);
-        Serial.print(sgp.serialnumber[1], HEX);
-        Serial.println(sgp.serialnumber[2], HEX);
-  }
-
-   
-   if (!sht4.begin()) {
-     Serial.println("Couldn't find SHT4x");
-   } else {
-     hasSensor = true;
-     Serial.println("Found SHT4x sensor");
-     Serial.print("Serial number 0x");
-     Serial.println(sht4.readSerial(), HEX);
-     if (bootCount < 2) {
-       // You can have 3 different precisions, higher precision takes longer
-       sht4.setPrecision(SHT4X_HIGH_PRECISION);
-       switch (sht4.getPrecision()) {
-       case SHT4X_HIGH_PRECISION:
-         Serial.println("High precision");
-         break;
-       case SHT4X_MED_PRECISION:
-         Serial.println("Med precision");
-         break;
-       case SHT4X_LOW_PRECISION:
-         Serial.println("Low precision");
-         break;
-       }
-       switch (sht4.getHeater()) {
-       case SHT4X_NO_HEATER:
-         Serial.println("No heater");
-         break;
-       case SHT4X_HIGH_HEATER_1S:
-         Serial.println("High heat for 1 second");
-         break;
-       case SHT4X_HIGH_HEATER_100MS:
-         Serial.println("High heat for 0.1 second");
-         break;
-       case SHT4X_MED_HEATER_1S:
-         Serial.println("Medium heat for 1 second");
-         break;
-       case SHT4X_MED_HEATER_100MS:
-         Serial.println("Medium heat for 0.1 second");
-         break;
-       case SHT4X_LOW_HEATER_1S:
-         Serial.println("Low heat for 1 second");
-         break;
-       case SHT4X_LOW_HEATER_100MS:
-         Serial.println("Low heat for 0.1 second");
-         break;
-       }
-     }
-   }
- }
-
- void initEEPROM() {
-   // Allocate The Memory Size Needed
-   EEPROM.begin(EEPROM_SIZE);
-
-   int seconds = EEPROM.readUInt(EEPROM_ADDRESS_TIME_TO_SLEEP);
-   if (seconds > TIME_TO_SLEEP) {
-     time_to_sleep_mode = seconds;
-   }
-
-   Serial.print("initEEPROM time_to_sleep_mode=");
-   Serial.println(time_to_sleep_mode);
-
-   deviceID = EEPROM.readString(EEPROM_ADDRESS_DEVICE_ID);
-   Serial.print("initEEPROM deviceID=");
-   Serial.println(deviceID);
-
-   serialNumber = EEPROM.readString(EEPROM_ADDRESS_SERIAL_NUMBER);
-   Serial.print("initEEPROM serialNumber=");
-   Serial.println(serialNumber);
- }
-
- void sendReport(bool hasReport) {
-   
-   String temperature = "0";
-   String relative_humidity = "0";
-   String  str_voc_index = "0";
-   if (hasSensor == true) {
-     sensors_event_t humidity, temp;
-     sht4.getEvent( & humidity, & temp);
-    
-     
-
-     temperature = String(temp.temperature, 2);
-     relative_humidity = String(humidity.relative_humidity, 2);
-
-     // https://github.com/adafruit/Adafruit_SGP40
        
-      uint16_t sraw;
-      float t  = temp.temperature;
-       Serial.print("Temp *C = "); Serial.print(t); Serial.print("\t\t");
-
-       float h = humidity.relative_humidity;
-       Serial.print("Hum. % = "); Serial.println(h);
-  
-      sraw = sgp.measureRaw(t,  h);
-      Serial.print("Raw measurement: ");
-      Serial.println(sraw);
-      
-      voc_index = sgp.measureVocIndex( t, h);
-      Serial.print("Voc Index: ");
-      Serial.println(voc_index);
-      str_voc_index = String(voc_index, 2);
-      
-   }
+        Serial.println(WiFi.localIP());
+        if (WiFi.status() != WL_CONNECTED && isCorrectPassword == false){
+          startLocalWeb();
+        } 
     
-   if ( hasReport == false) {
-     return;
-   }
-
-    initWiFi();
+ 
     if (WiFi.status() != WL_CONNECTED  ) {
-     return;
-   }
-
-     String localIP =  WiFi.localIP().toString();
-   if ( localIP== "0.0.0.0") {
+      Serial.println("sendReport WiFi.status() != WL_CONNECTED");
+      WiFi.disconnect();
+      WiFi.mode(WIFI_OFF); 
+      return;
+    }
+    
+    String localIP =  WiFi.localIP().toString();
+    if ( localIP== "0.0.0.0") {
       Serial.println("sendReport  localIP= 0.0.0.0");
-      turnOffWiFi();
-     return;
-   }
-   
-   WiFiClientSecure * client = new WiFiClientSecure;
-   if (!client) {
-    turnOffWiFi();
-     return;
-   }
+      WiFi.disconnect();
+      WiFi.mode(WIFI_OFF); 
+      return;
+    }
+    
+    WiFiClientSecure * client = new WiFiClientSecure;
+    if (!client) {
+      WiFi.disconnect();
+      WiFi.mode(WIFI_OFF); 
+      return;
+    }
 
    client -> setInsecure();
    HTTPClient http;
@@ -553,8 +431,130 @@ void startLocalWeb(){
    // Free resources
    http.end();
    delete client;
+    WiFi.disconnect();
+ }
+ 
+ void initSht4x() {
+   Serial.println("Telua SHT4x test");
+    
+  if (! sgp.begin()){
+    Serial.println("SGP40 sensor not found :(");
+    return;
+  }else{ 
+        Serial.print("Found SGP40 serial #");
+        Serial.print(sgp.serialnumber[0], HEX);
+        Serial.print(sgp.serialnumber[1], HEX);
+        Serial.println(sgp.serialnumber[2], HEX);
+  }
 
-   turnOffWiFi();
+   
+   if (!sht4.begin()) {
+     Serial.println("Couldn't find SHT4x");
+   } else {
+     hasSensor = true;
+     Serial.println("Found SHT4x sensor");
+     Serial.print("Serial number 0x");
+     Serial.println(sht4.readSerial(), HEX);
+//     if (bootCount < 2) {
+//       // You can have 3 different precisions, higher precision takes longer
+//       sht4.setPrecision(SHT4X_HIGH_PRECISION);
+//       switch (sht4.getPrecision()) {
+//       case SHT4X_HIGH_PRECISION:
+//         Serial.println("High precision");
+//         break;
+//       case SHT4X_MED_PRECISION:
+//         Serial.println("Med precision");
+//         break;
+//       case SHT4X_LOW_PRECISION:
+//         Serial.println("Low precision");
+//         break;
+//       }
+//       switch (sht4.getHeater()) {
+//       case SHT4X_NO_HEATER:
+//         Serial.println("No heater");
+//         break;
+//       case SHT4X_HIGH_HEATER_1S:
+//         Serial.println("High heat for 1 second");
+//         break;
+//       case SHT4X_HIGH_HEATER_100MS:
+//         Serial.println("High heat for 0.1 second");
+//         break;
+//       case SHT4X_MED_HEATER_1S:
+//         Serial.println("Medium heat for 1 second");
+//         break;
+//       case SHT4X_MED_HEATER_100MS:
+//         Serial.println("Medium heat for 0.1 second");
+//         break;
+//       case SHT4X_LOW_HEATER_1S:
+//         Serial.println("Low heat for 1 second");
+//         break;
+//       case SHT4X_LOW_HEATER_100MS:
+//         Serial.println("Low heat for 0.1 second");
+//         break;
+//       }
+//     }
+   }
+ }
+
+ void initEEPROM() {
+   // Allocate The Memory Size Needed
+   EEPROM.begin(EEPROM_SIZE);
+
+   int seconds = EEPROM.readUInt(EEPROM_ADDRESS_TIME_TO_SLEEP);
+   if (seconds > TIME_TO_SLEEP) {
+     time_to_sleep_mode = seconds;
+   }
+
+   Serial.print("initEEPROM time_to_sleep_mode=");
+   Serial.println(time_to_sleep_mode);
+
+   deviceID = EEPROM.readString(EEPROM_ADDRESS_DEVICE_ID);
+   Serial.print("initEEPROM deviceID=");
+   Serial.println(deviceID);
+
+   serialNumber = EEPROM.readString(EEPROM_ADDRESS_SERIAL_NUMBER);
+   Serial.print("initEEPROM serialNumber=");
+   Serial.println(serialNumber);
+ }
+
+ void readSensor(bool hasReport) {
+   String temperature = "0";
+   String relative_humidity = "0";
+   String  str_voc_index = "0";
+   if (hasSensor == true) {
+     sensors_event_t humidity, temp;
+     sht4.getEvent( & humidity, & temp);
+    
+     
+
+     temperature = String(temp.temperature, 2);
+     relative_humidity = String(humidity.relative_humidity, 2);
+
+     // https://github.com/adafruit/Adafruit_SGP40
+       
+      uint16_t sraw;
+      float t  = temp.temperature;
+       Serial.print("Temp *C = "); Serial.print(t); Serial.print("\t\t");
+
+       float h = humidity.relative_humidity;
+       Serial.print("Hum. % = "); Serial.println(h);
+  
+      sraw = sgp.measureRaw(t,  h);
+      Serial.print("Raw measurement: ");
+      Serial.println(sraw);
+      
+      int32_t   voc_index = sgp.measureVocIndex( t, h);
+      Serial.print("Voc Index: ");
+      Serial.println(voc_index);
+      led_index = voc_index;
+      str_voc_index = String(voc_index, 2);
+      
+   }
+    
+   if ( hasReport == false) {
+     return;
+   }
+    reportCloud( temperature,  relative_humidity, str_voc_index);
  }
  /*
  Method to print the reason by which ESP32
@@ -564,39 +564,39 @@ void startLocalWeb(){
  void setup() {
    Serial.begin(115200);
    delay(1000); //Take some time to open up the Serial Monitor
-
-   //Increment boot number and print it every reboot
-   ++bootCount;
-   Serial.println("Boot number: " + String(bootCount));
-
+ 
    initEEPROM();
 
    intGpio();
    
    initSht4x();
 
-   sendReport(true);
+   readSensor(true);
 
     
  }
 
  void loop() {
    //This is not going to be called
-      for(int i= 0; i < time_to_sleep_mode ; i++){
-          sendReport(false);
-           if(voc_index > 400){
+   // time_to_sleep_mode
+      for(int i= 0; i < 10 ; i++){
+          Serial.print(" i=");
+          Serial.print(i);
+          readSensor(false);
+          
+          if(led_index > 400){
             turnOnNotification(3);
           }
-          else  if(voc_index > 200){
+          else  if(led_index > 200){
             turnOnNotification(2);
           }
-          else if(voc_index > 100){
+          else if(led_index > 100){
             turnOnNotification(1);
           }else{
             turnOnNotification(0);
           }
       }
 
-      sendReport(true);
+      readSensor(true);
       delay(1000);
  }
