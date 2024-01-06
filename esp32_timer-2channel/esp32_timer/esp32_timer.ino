7e0f722f1ca8335e6ba271fc7effc965b060fb8f
Thong LT <letrthong@gmail.com>
2024-01-06 18:12:35 +0700

    Update esp32_timer.ino

--- esp32_timer-2channel/esp32_timer/esp32_timer.ino
+++ esp32_timer-2channel/esp32_timer/esp32_timer.ino
@@ -929,6 +929,7 @@ int getSeconds(){
 void setup() {
 
   Serial.begin(115200);
+   delay(1000); 
   Serial.println("Serial baudRate=115200");
   delay(1000); //Take some time to open up the Serial Monitor
   Serial.println("Ver:8/Aug/2023");
