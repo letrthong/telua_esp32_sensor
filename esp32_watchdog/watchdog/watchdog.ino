fcad43d07c15a82a6063267bf659bce45ca4e1fc
Thong LT <letrthong@gmail.com>
2024-08-11 00:37:48 +0700

    Update watchdog.ino

--- esp32_watchdog/watchdog/watchdog.ino
+++ esp32_watchdog/watchdog/watchdog.ino
@@ -2,34 +2,47 @@
 
                  
 // WDT Timeout in seconds
+TaskHandle_t taskHandle;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Setup started.");
   delay(2000);
   esp_task_wdt_config_t  config;
-  config.timeout_ms =5000;
+  config.timeout_ms = (5 * 1000);
   config.trigger_panic = true;
+
+    
   
   esp_task_wdt_init(&config); // Initialize ESP32 Task WDT
   esp_task_wdt_add(NULL);               // Subscribe to the Task WDT
+
+
+  // Create task1
+  xTaskCreate(
+    task1,       // Task function pointer
+    "Task1",     // Task name
+    2000,        // Stack depth in words
+    NULL,        // Task parameter
+    2,           // Task priority
+    &taskHandle  // Task handle
+  );
 }
 
 void loop() {
-  Serial.println("LOOP started ! ");
-  for (int i = 0; i <= 10; i++) {
-    Serial.print("Task: ");
-    Serial.println(i);
+    Serial.println("LOOP started ! ");
     delay(1000);
     // Kick the dog
     esp_task_wdt_reset();
-  }
+}
 
-  int count = 0;
+
+void task1(void *parameter) {
+   int count = 0;
   while (1) {
     Serial.print("MCU hang event!!!: ");
     Serial.println(count);
     count = count+ 1;
-    delay(1000);
+    delay(10000);
   }
 }
