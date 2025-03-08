cb5d93bd5c8c1491147e46a07ac85b432219165d
Thong LT <letrthong@gmail.com>
2025-03-08 12:40:47 +0700

    x

--- esp32_STC-013/esp32_sct-013/esp32_sct-013.ino
+++ esp32_STC-013/esp32_sct-013/esp32_sct-013.ino
@@ -1,17 +1,41 @@
+// https://lastminuteengineers.com/esp32-basics-adc/
 // Potentiometer is connected to GPIO 34 (Analog ADC1_CH6) 
 const int potPin = 34;
 
 // variable for storing the potentiometer value
-int potValue = 0;
-
+int potPin_adc = 0;
+int count = 0 ;
+// 2^12
+int  volt_input = 3.3;
+int max_value =  4096;
 void setup() {
+  
   Serial.begin(115200);
   delay(1000);
+   Serial.println("ADC1_CH6");
 }
 
 void loop() {
-  // Reading potentiometer value
-  potValue = analogRead(potPin);
-  Serial.println(potValue);
-  delay(500);
+  //get ADC value for a given pin/ADC channel in millivolts.
+  potValue = analogReadMilliVolts(potPin_adc);
+  if(potValue > 150){
+    Serial.println(potValue);
+    count = 0;
+    float currentValue = (potValue*100)/3300;
+    Serial.println(currentValue);
+  }
+  else
+  {
+      if(count > 150){
+          count = count;
+           Serial.println("ADC1_CH6");
+      }
+      
+  }
+
+
+  count = count +1;
+
+  
+  delay(100);
 }
